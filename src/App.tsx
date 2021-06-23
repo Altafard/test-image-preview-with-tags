f460a4f1a8a864e0cee030cdd3905852b88b3826
Alexander Pilipenko <altafard@gmail.com>
2021-06-23 15:18:23 +0300

    Implement uploading and resizing image

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import useWindowSize from './hooks/useWindowSize';
 
-function App() {
+export default function App() {
+  const {width, height} = useWindowSize();
+  const [file, setFile] = useState({
+    url: ""
+  });
+
+  function handleChange(event: any) {
+    setFile({
+      url: URL.createObjectURL(event.target.files[0])
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="WindowSize">{width} &bull; {height}</div>
+      <div hidden={file.url.length !== 0}>
+        <input type="file" onChange={handleChange} />
+      </div>
+      <div hidden={file.url.length === 0}>
+        <img src={file.url} style={{ maxWidth: width, maxHeight: height }} />
+      </div>
     </div>
   );
 }
-
-export default App;
